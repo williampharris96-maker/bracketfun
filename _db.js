8b260592ee5fb630c8cc51bc04a94e5a14731515
williampharris96-maker <williampharris96@gmail.com>
2026-03-03 22:38:53 -0600

    Delete _db.js

--- _db.js
+++
@@ -1,9 +0,0 @@
-const { neon } = require('@neondatabase/serverless');
-
-let sql;
-function getDb() {
-  if (!sql) sql = neon(process.env.DATABASE_URL);
-  return sql;
-}
-
-module.exports = { getDb };
